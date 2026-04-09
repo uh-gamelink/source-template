dd710820fa1b7ebf3d9d1e527f1e7e615ddfae68
johngabrielmartinez <jgtm@hawaii.edu>
2026-04-09 01:47:55 -1000

    fix signout redirect

--- src/app/auth/signout/page.tsx
+++ src/app/auth/signout/page.tsx
@@ -10,7 +10,7 @@ const SignOut = () => (
     <Row>
       <Col xs={4} />
       <Col>
-        <Button variant="danger" onClick={() => signOut({ callbackUrl: '/', redirect: true })}>
+        <Button variant="danger" onClick={() => signOut({ callbackUrl: '/'})}>
           Sign Out
         </Button>
       </Col>
