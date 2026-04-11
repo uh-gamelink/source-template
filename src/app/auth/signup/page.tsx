61231ef7f6a8a05059d8d9a11615cab5acebeecf
Tuan Do <minhtuando707@gmail.com>
2026-04-10 17:44:56 -1000

    Create a "Create a Profile" page

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -39,8 +39,8 @@ const SignUp = () => {
   const onSubmit = async (data: SignUpForm) => {
     // console.log(JSON.stringify(data, null, 2));
     await createUser(data);
-    // After creating, signIn with redirect to the add page
-    await signIn('credentials', { callbackUrl: '/add', ...data });
+    // After creating, signIn with redirect to Create Profile page
+    await signIn('credentials', { callbackUrl: '/auth/create-profile', ...data });
   };
 
   return (
