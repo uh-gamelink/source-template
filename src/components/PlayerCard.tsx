7e0a9fa4ec9ed67532186a5cdd1df44ed1030ce3
ellaself <ellaself@hawaii.edu>
2026-04-14 09:15:24 -1000

    updated player card

---
+++ src/components/PlayerCard.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Card, Image } from 'react-bootstrap';
+
+type Player = {
+  username: string;
+  imageUrl?: string | null;
+  game: string;
+  rank: string;
+};
+
+const PlayerCard = ({ player }: { player: Player }) => (
+  <Card style={{ width: '300px'}}>
+    <Card.Header>
+      <Image
+        src={player.imageUrl || '/default-player.png'}
+        width={250}
+        height={250}
+        rounded
+        alt={player.username}
+        className="card-body d-flex justify-content-center align-items-center">
+      </Image>
+    </Card.Header>
+
+    <Card.Body>
+      <Card.Title>{player.username}</Card.Title>
+      <Card.Text>
+        <div>Game: {player.game}</div>
+        <div>Rank: {player.rank}</div>
+      </Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default PlayerCard;
