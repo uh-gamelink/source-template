521c89af5237626a8bb6f9bfb3166945b365196e
Tuan Do <minhtuando707@gmail.com>
2026-04-14 22:16:53 -1000

    Change redirect link after signup

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -40,7 +40,7 @@ const SignUp = () => {
     // console.log(JSON.stringify(data, null, 2));
     await createUser(data);
     // After creating, signIn with redirect to the add page
-    await signIn('credentials', { callbackUrl: '/create-profile', ...data });
+    await signIn('credentials', { callbackUrl: '/profile/edit', ...data });
   };
 
   return (
