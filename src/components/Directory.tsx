9a40b7f0a87c81f463dcc0703a517c22f89402d5
ellaself <ellaself@hawaii.edu>
2026-04-18 20:49:34 -1000

    added links

--- src/components/Directory.tsx
+++ src/components/Directory.tsx
@@ -1,14 +1,22 @@
-  import { Col, Container, Card } from 'react-bootstrap';
+  import { Col, Container, Card, Button } from 'react-bootstrap';
   
   /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
   const Directory = () => (
-    <footer className="mt-auto py-3">
       <Container className="mt-4">
       <Col>
         <Card>
          <Card.Body>
             <Card.Text>Visit our community page to exploring the dicord game servers on campus</Card.Text>
-            <Card.Text> connect </Card.Text>
+              <div className="mt-auto">
+                <Button
+                  href="/src/community"
+                  target="Community"
+                  rel="_blank"
+                  className="custom-tag-btn border-0"
+                >
+                  Connect
+                </Button>
+              </div>
           </Card.Body>
          </Card>
       </Col>
@@ -16,7 +24,16 @@
         <Card>
          <Card.Body>
             <Card.Text> See our about page to get to know the developers of UH GameLink</Card.Text>
-            <Card.Text> visit </Card.Text>
+              <div className="mt-auto">
+                <Button
+                  href="/src/app/about"
+                  target="About"
+                  rel="_blank"
+                  className="custom-tag-btn border-0"
+                >
+                  Visit
+                </Button>
+              </div>
           </Card.Body>
          </Card>
       </Col>
@@ -24,12 +41,20 @@
         <Card>
          <Card.Body>
             <Card.Text>Interested in joining the UH game community, go to our sign up page to connect with players on campus</Card.Text>
-            <Card.Text>Sign up</Card.Text>
+              <div className="mt-auto">
+                <Button
+                  href="/auth/app/signin"
+                  target="Signup"
+                  rel="_blank"
+                  className="custom-tag-btn border-0"
+                >
+                  Sign up
+                </Button>
+              </div>  
           </Card.Body>
          </Card>
       </Col>
      </Container>
-    </footer>
   );
   
   export default Directory;
