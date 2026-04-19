053061d955cc68c55a05a2776de32ee179cb512e
ellaself <ellaself@hawaii.edu>
2026-04-18 20:53:50 -1000

    created Directory segment

---
+++ src/components/PlayerCard 2.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Card, Container, Image } from 'react-bootstrap';
+
+type Player = {
+  username: string;
+  imageUrl?: string | null;
+  game: string;
+  rank: string;
+};
+
+const PlayerCard = ({ player }: { player: Player }) => (
+<Card className="h-100 custom-card-body" style={{ width: '235px' }}>
+  <Container className="ms-2">
+  <Card.Body className="p-3">
+    <Card.Title className="pb-2">{player.username}</Card.Title>
+    <Image
+      src={player.imageUrl || '/default-player.png'}
+      width={155}
+      height={160}
+      rounded
+      alt={player.username}
+      style={{
+        objectFit: 'cover',
+        borderRadius: '12px',
+        border: '4px solid #b0e0e682',
+      }}
+      className="mb-3"
+    />
+
+    <div>Game: {player.game}</div>
+    <div>Rank: {player.rank}</div>
+
+    <a className="pb-2" href="/auth/signin">
+      Connect
+    </a>
+
+  </Card.Body>
+  </Container>
+</Card>
+);
+
+export default PlayerCard;
