a10e60681a0638a8d5df545a73fe3f2e367c9d5c
ellaself <ellaself@hawaii.edu>
2026-04-19 12:08:07 -1000

    updated the add button

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Card, Container, Image, Button } from 'react-bootstrap';
+import { Card, Container, Image, Row, Col } from 'react-bootstrap';
 import { PlusLg } from 'react-bootstrap-icons';
 
 type Player = {
@@ -14,11 +14,17 @@ const PlayerCard = ({ player }: { player: Player }) => (
 <Card className="h-100 custom-card-body" style={{ width: '235px' }}>
   <Container className="ms-2">
     <Card.Body className="p-3">
-      <Card.Title className='pb-2'>
-        <a href="/findplayers/request" className="custom-link py-2">
-        <PlusLg /> 
-        {player.username}
-      </a>
+      <Card.Title>
+        <Row className="ps-0 py-2">
+          <Col md={1}>
+            <a href="/findplayers/request" className='custom-link-button'>
+              <PlusLg/> 
+            </a>
+          </Col>
+          <Col md={10} className="username-size">
+            {player.username}
+          </Col>
+        </Row>
       </Card.Title>
       <Image
         src={player.imageUrl || '/default-player.png'}
