50bfb27218f67289e33e3de3e7b2ec0d7b66dcfb
ellaself <ellaself@hawaii.edu>
2026-04-19 11:38:51 -1000

    added plus button

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { Card, Container, Image } from 'react-bootstrap';
+import { Card, Container, Image, Button } from 'react-bootstrap';
+import { PlusLg } from 'react-bootstrap-icons';
 
 type Player = {
   username: string;
@@ -12,30 +13,29 @@ type Player = {
 const PlayerCard = ({ player }: { player: Player }) => (
 <Card className="h-100 custom-card-body" style={{ width: '235px' }}>
   <Container className="ms-2">
-  <Card.Body className="p-3">
-    <Card.Title className="pb-2">{player.username}</Card.Title>
-    <Image
-      src={player.imageUrl || '/default-player.png'}
-      width={155}
-      height={160}
-      rounded
-      alt={player.username}
-      style={{
-        objectFit: 'cover',
-        borderRadius: '12px',
-        border: '4px solid #b0e0e682',
-      }}
-      className="mb-3"
-    />
-
-    <div>Game: {player.game}</div>
-    <div>Rank: {player.rank}</div>
-
-    <a className="pb-2" href="/auth/signin">
-      Connect
-    </a>
-
-  </Card.Body>
+    <Card.Body className="p-3">
+      <Card.Title className='pb-2'>
+        <a href="/findplayers/request" className="custom-link py-2">
+        <PlusLg /> 
+        {player.username}
+      </a>
+      </Card.Title>
+      <Image
+        src={player.imageUrl || '/default-player.png'}
+        width={155}
+        height={160}
+        rounded
+        alt={player.username}
+        style={{
+          objectFit: 'cover',
+          borderRadius: '12px',
+          border: '4px solid #b0e0e682',
+        }}
+        className="mb-3"
+      />
+      <div>Game: {player.game}</div>
+      <div>Rank: {player.rank}</div>
+    </Card.Body>
   </Container>
 </Card>
 );
