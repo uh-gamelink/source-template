d66c767308ad3710dc9865c69ef9d3efdf134506
ellaself <ellaself@hawaii.edu>
2026-04-21 14:28:25 -1000

    updated the player card

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import { Card, Container, Image, Row, Col, Button } from 'react-bootstrap';
 import { PlusLg } from 'react-bootstrap-icons';
 
@@ -9,14 +8,19 @@ type Player = {
   rank: string;
 };
 
-const PlayerCard = ({ player }: { player: Player }) => (
+type Props = {
+  player: Player;
+  onRequestClick: (player: Player) => void;
+};
+
+const PlayerCard = ({ player, onRequestClick }: Props) => (
   <Card className="h-100 custom-card-body" style={{ width: '235px' }}>
     <Container className="ms-2">
       <Card.Body className="p-3">
         <Card.Title>
           <Col className="py-3">
-              {player.username}
-            </Col>
+            {player.username}
+          </Col>
         </Card.Title>
 
         <Image
@@ -35,16 +39,26 @@ const PlayerCard = ({ player }: { player: Player }) => (
 
         <div>Game: {player.game}</div>
         <div className="mb-2">Rank: {player.rank}</div>
+
         <Row className="ps-0 py-2 align-items-center">
           <Col xs="auto">
-            <Link href={`/requests?game=${encodeURIComponent(player.game)}&rank=${encodeURIComponent(player.rank)}`}  className="custom-link-button">
-                <PlusLg />
-            </Link>
+            <Button
+              variant="link"
+              className="custom-link-button p-0"
+              onClick={() => onRequestClick(player)}
+            >
+              <PlusLg />
+            </Button>
           </Col>
-          <Col className= "pl-0">
-            <Link href={`/requests?game=${encodeURIComponent(player.game)}&rank=${encodeURIComponent(player.rank)}`} >
-              <Button className="custom-tag-btn" size="sm">Connect</Button>
-            </Link>
+
+          <Col className="pl-0">
+            <Button
+              className="custom-tag-btn"
+              size="sm"
+              onClick={() => onRequestClick(player)}
+            >
+              Connect
+            </Button>
           </Col>
         </Row>
       </Card.Body>
