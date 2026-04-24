f7512b2edb2c5611a4696cbe2baf1d6137dd50d5
Ella Self <142930889+ellaself@users.noreply.github.com>
2026-04-23 22:59:53 -1000

    Create page.tsx

---
+++ src/app/gamelibrary/favorites/page.tsx
@@ -0,0 +1,7 @@
+import FavoritesClient from '@/app/gamelibrary/favorites/FavoritesClient';
+
+export const dynamic = 'force-dynamic';
+
+const FavoritesPage = () => <FavoritesClient />;
+
+export default FavoritesPage;
