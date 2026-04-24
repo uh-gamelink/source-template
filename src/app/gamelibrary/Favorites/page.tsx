4a6fdf559c3c2d0d80a02b0131b7021d61b5572e
Ella Self <142930889+ellaself@users.noreply.github.com>
2026-04-23 23:00:20 -1000

    Delete src/app/gamelibrary/Favorites directory

--- src/app/gamelibrary/Favorites/page.tsx
+++
@@ -1,7 +0,0 @@
-import FavoritesClient from '@/app/gamelibrary/favorites/FavoritesClient';
-
-export const dynamic = 'force-dynamic';
-
-const FavoritesPage = () => <FavoritesClient />;
-
-export default FavoritesPage;
